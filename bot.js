a6367d512941019e934bcfc963b8de83f563c152
0amr-ashraf0 <46089737+0amr-ashraf0@users.noreply.github.com>
2018-12-27 19:08:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,23 +81,7 @@ if (message.content === '!spam') {
       }
 });
 
-client3.on('message', message => { // لا تغير شئ لكي ما تتخرب
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Baldiiiiii , Farm Credit By Baldiiiiii , ez Farm - كردت اسبام اسبام اسبام اوف ءف نار نار نار نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.TOKEN3);//لا تغير فيها شي
